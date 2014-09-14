87f802a4356e9b52ee6ca0281b093d0465050d53
catatsuy <catatsuy@catatsuy.org>
2014-09-14 19:45:55 +0900

    投稿一覧を表示する

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'sqlite3'
+require 'pp'
 
 db = SQLite3::Database.new "db/post.db"
 db.results_as_hash = true
 
 get '/' do
+  posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+  pp posts
   erb :index
 end
 
