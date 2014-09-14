f2ac302cfd91e1e6af2d8449b35bc3803aa63aa9
catatsuy <catatsuy@catatsuy.org>
2014-09-14 19:48:41 +0900

    投稿をHTMLに表示

--- main.rb
+++ main.rb
@@ -7,8 +7,7 @@ db.results_as_hash = true
 
 get '/' do
   posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
-  pp posts
-  erb :index
+  erb :index, { :locals => { :posts => posts } }
 end
 
 post '/' do
