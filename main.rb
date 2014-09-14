a476475948114db006f7876830f19cfb54ca5597
catatsuy <catatsuy@catatsuy.org>
2014-09-14 19:57:45 +0900

    投稿された画像を保存

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'sqlite3'
-require 'pp'
+require 'securerandom'
 
 db = SQLite3::Database.new "db/post.db"
 db.results_as_hash = true
@@ -11,8 +11,31 @@ get '/' do
 end
 
 post '/' do
-  stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
-  stmt.bind_params(params["ex_text"])
+  file_name = ""
+
+  if params["file"]
+    ext = ""
+    if params["file"][:type].include? "jpeg"
+      ext = "jpg"
+    elsif params["file"][:type].include? "png"
+      ext = "png"
+    else
+      return "投稿できる画像形式はjpgとpngだけです"
+    end
+
+    # 適当なファイル名を付ける
+    file_name = SecureRandom.hex + "." + ext
+
+    # 画像を保存
+    File.open("./public/uploads/" + file_name, 'wb') do |f|
+      f.write params["file"][:tempfile].read
+    end
+  else
+    return "画像が必須です"
+  end
+
+  stmt = db.prepare("INSERT INTO posts (text, img_file_name) VALUES (?, ?)")
+  stmt.bind_params(params["ex_text"], file_name)
   stmt.execute
   redirect '/'
 end
