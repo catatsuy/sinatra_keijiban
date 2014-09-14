fc7f1c3628da5ed211d0be8c2001c90eb02dbe22
catatsuy <catatsuy@catatsuy.org>
2014-09-14 19:39:12 +0900

    投稿をSQLiteに保存するように

--- main.rb
+++ main.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new "db/post.db"
+db.results_as_hash = true
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  p params["ex_text"]
+  stmt = db.prepare("INSERT INTO posts (text) VALUES (?)")
+  stmt.bind_params(params["ex_text"])
+  stmt.execute
+  redirect '/'
 end
 
 get '/hello' do
